5771814e441b055f0ca7ba8e1bf5f0af495371e0
mir <mir3732@live.com>
2018-08-30 11:26:54 +0800

    Test timing task.

--- timed-task/src/main/java/com/mir/tasks/Jobs.java
+++ timed-task/src/main/java/com/mir/tasks/Jobs.java
@@ -12,17 +12,38 @@ public class Jobs {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    /**
+     * 从程序启动时间开始，一分钟执行一次
+     * 执行结果如下：
+     2018-08-30 11:21:45 >>fixedDelay执行....
+     2018-08-30 11:22:45 >>fixedDelay执行....
+     2018-08-30 11:23:45 >>fixedDelay执行....
+     */
     @Scheduled(fixedDelay = ONE_MINUTE)
     public void fixedDelayJob() {
         System.out.println(FORMATTER.format(LocalDateTime.now()) + " >>fixedDelay执行....");
     }
 
+    /**
+     * 从程序启动时间开始，一分钟执行一次
+     * 执行结果如下：
+     2018-08-30 11:21:45 >>fixedRate执行....
+     2018-08-30 11:22:45 >>fixedRate执行....
+     2018-08-30 11:23:45 >>fixedRate执行....
+     */
     @Scheduled(fixedRate = ONE_MINUTE)
     public void fixedRateJob() {
         System.out.println(FORMATTER.format(LocalDateTime.now()) + " >>fixedRate执行....");
     }
 
-    @Scheduled(cron = "0 30 * * * ?")
+    /**
+     * 一分钟执行一次
+     * 执行结果如下：
+     * 2018-08-30 11:22:00 >>cron执行....
+     * 2018-08-30 11:23:00 >>cron执行....
+     * 2018-08-30 11:24:00 >>cron执行....
+     */
+    @Scheduled(cron = "0 0/1 * * * ?")
     public void cronJob() {
         System.out.println(FORMATTER.format(LocalDateTime.now()) + " >>cron执行....");
     }
